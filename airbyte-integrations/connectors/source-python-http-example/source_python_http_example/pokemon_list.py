2cb6815d1da0e0c7b87349f8dead60fadfea1836
Dhruv97Sharma <ddhuv.sharma1997@gmail.com>
2022-01-08 23:56:15 +0530

    Added pokemonapi connector http incremental sync for ETL pipeline data

---
+++ airbyte-integrations/connectors/source-python-http-example/source_python_http_example/pokemon_list.py
@@ -0,0 +1,20 @@
+'''
+Contains valid pokemons to be included in the list
+'''
+
+POKEMON_LIST = [
+    "pikachu",
+    "nidoking",
+    "nidoqueen",
+    "psyduck",
+    "jigglypuff",
+    "charmander",
+    "charmeleon",
+    "charizard",
+    "bulbasaur",
+    "ivasaur",
+    "venasaur",
+    "squirtle",
+    "waterturtle",
+    "blastoise",
+]
